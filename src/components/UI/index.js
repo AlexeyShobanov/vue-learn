65ba5beedb007e16cdb2b93f03204ca4211f6d94
Alexey Shobanov <shobanov@gmail.com>
2023-08-30 18:00:09 +0300

    add sort with watch

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -1,4 +1,6 @@
 import MyButton from "@/components/UI/MyButton";
 import MyInput from "@/components/UI/MyInput";
+import MyDialog from "@/components/UI/MyDialog";
+import MySlelect from "@/components/UI/MySlelect";
 
-export default [MyButton, MyInput];
+export default [MyButton, MyInput, MyDialog, MySlelect];
