b719cdfb2e2717f2fba7839ecc049498eefa6022
Alexey Shobanov <shobanov@gmail.com>
2023-09-20 10:39:27 +0300

    composition api

--- src/pages/PostPageCompositionApi.vue
+++ src/pages/PostPageCompositionApi.vue
@@ -1,59 +1,58 @@
 <template>
   <div>
     <h1>Станица с постами</h1>
-    <h1>{{ likes }}</h1>
-    <my-button @click="addLike">Add Like</my-button>
-    <!--    <my-input v-focus v-model="searchQuery" placeholder="Поиск..."></my-input>-->
-    <!--    <div class="app__btns">-->
-    <!--      <my-button @click="showDialog">Создать пост</my-button>-->
-    <!--      <my-select v-model="selectedSort" :options="sortOptions"></my-select>-->
-    <!--    </div>-->
-    <!--    <my-dialog v-model:show="dialogVisible">-->
-    <!--      <post-form @create="createPost"></post-form>-->
-    <!--    </my-dialog>-->
-    <!--    <post-list-->
-    <!--      :posts="sortedAndSearchedPosts"-->
-    <!--      @remove="removePost"-->
-    <!--      v-if="!isPostsLoading"-->
-    <!--    ></post-list>-->
-    <!--    <div v-else>Идет загрузка...</div>-->
-    <!--    &lt;!&ndash;    <div ref="observer" class="observer"></div>&ndash;&gt;-->
-    <!--    <div v-intersection="loadMorePosts" class="observer"></div>-->
+    <my-input v-focus v-model="searchQuery" placeholder="Поиск..."></my-input>
+    <div class="app__btns">
+      <my-button>Создать пост</my-button>
+      <my-select v-model="selectedSort" :options="sortOptions"></my-select>
+    </div>
+    <my-dialog v-model:show="dialogVisible"> </my-dialog>
+    <post-list
+      :posts="sortedAndSearchedPosts"
+      v-if="!isPostsLoading"
+    ></post-list>
+    <div v-else>Идет загрузка...</div>
   </div>
 </template>
 
 <!--В секции script нужно экспортировать сам объект, описываемый в файле, по сути все остальное ситксический сахар-->
 <script>
-// import PostForm from "@/components/PostForm";
-// import PostList from "@/components/PostList";
-import { ref } from "vue";
+import PostList from "@/components/PostList";
+import { usePosts } from "@/hooks/usePosts";
+import useSortedPosts from "@/hooks/useSortedPosts";
+import useSortedAndSearchedPosts from "@/hooks/useSortedAndSearchedPosts";
 
 export default {
   components: {
-    // PostList,
-    // PostForm,
+    PostList,
   },
   data() {
     return {
       dialogVisible: false,
       sortOptions: [
         { value: "title", name: "По названию" },
-        { value: "body", name: "По содержанию" },
+        { value: "body", name: "По содержимому" },
       ],
     };
   },
-  methods: {},
-  mounted() {},
-  computed: {},
   setup(props) {
-    const likes = ref(0);
+    const { posts, totalPages, isPostsLoading } = usePosts(10);
 
-    const addLike = () => {
-      likes.value += 1;
-    };
+    const { sortedPosts, selectedSort } = useSortedPosts(posts);
+    const { searchQuery, sortedAndSearchedPosts } =
+      useSortedAndSearchedPosts(sortedPosts);
 
     console.log(props);
-    return { likes, addLike };
+
+    return {
+      posts,
+      totalPages,
+      isPostsLoading,
+      sortedPosts,
+      selectedSort,
+      searchQuery,
+      sortedAndSearchedPosts,
+    };
   },
 };
 </script>
@@ -63,10 +62,5 @@ export default {
   margin: 15px 0;
   display: flex;
   justify-content: space-between;
-}
-
-.observer {
-  height: 10px;
-  /*background: teal;*/
 }
 </style>
